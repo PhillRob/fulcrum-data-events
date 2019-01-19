5ece4424fd3cd964c395a2f5d6ffaba0e74f6159
PhillRob <robeck-p@email.ulster.ac.uk>
2019-01-19 08:30:47 +0300

    inital

---
+++ WH-tree-monitoring.js
@@ -0,0 +1,400 @@
+var config = {
+        //auto_sync_enabled: true,
+        //auto_location_enabled: true,
+        auto_location_minimum_accuracy: 20
+        //manual_location_enabled: false,
+        //media_gallery_enabled: false,
+        //media_capture_enabled: true,
+        //photo_quality: '2048',
+        //video_quality: '720p',
+        //drafts_enabled: false,
+        //edit_locations_enabled: true,
+        //edit_durations_enabled: true
+        };
+ SETFORMATTRIBUTES(config);
+
+//change status to 'action required' when maintenance action is not empty
+//function changeStatus(event) {
+//  if (event.value) {
+//    // There is a value, so set the status
+//    SETSTATUS('1')
+//  } else {
+//   // There is no value. It could have been cleared from the field.
+//    // Revert status to 'created'.
+//    SETSTATUS('2')
+//  }
+//}
+//ON('change', 'maintenance_action', changeStatus);
+
+
+//allow user [ADA] Site Inspector to NOT create record
+ON('new-record', function(event) {
+ 	var adminRoles = ['Inspector','Owner'];
+  	var rangerRoles = ['[ADA] Site Inspector'];
+  
+	// if current role is not admin, inspector but [ADA] Site
+	if (rangerRoles.indexOf(ROLE()) !== -1) 
+    {
+    	// ranger fields
+		//SETHIDDEN('km', true);
+      	//SETHIDDEN('tree_no', true);
+       	//SETHIDDEN('status', true);
+        //SETHIDDEN('species', true);
+        //SETHIDDEN('planted_tree', true);
+        //SETHIDDEN('height_m', true);
+      	//SETHIDDEN('spread_m', true);
+      	//SETHIDDEN('height_m', true);
+      	//SETHIDDEN('dbh_cm', true);
+		//SETHIDDEN('health',true);
+        //SETHIDDEN('indicator',true);
+		//SETHIDDEN('health_re_inspection',true);
+        //SETHIDDEN('irrigation_present',true);
+      	//SETHIDDEN('structure',true);
+      	//SETSTATUSHIDDEN(true);
+      	//SETHIDDEN('impact_of_ground_water',true);
+       	//SETHIDDEN('maintenance_action',true);
+        //SETHIDDEN('comments',true);
+		//SETHIDDEN('photos', true);
+		//ALERT('Sorry!', 'You cannot create a record in this app. ');
+    	//INVALID('Sorry! You are not allowed to create records in this app.');
+	}
+  	// if the current role is one of the designated admin roles...
+  	if (adminRoles.indexOf(ROLE()) !== -1) 
+    {
+	//do nothing
+  	}
+  
+});
+
+//allow [ADA] Site Inspector to only change status to orange if near record location
+ON('validate-record', function(event) 
+   {
+ 	var adminRoles = ['Inspector','Owner'];
+  	var rangerRoles = ['[ADA] Site Inspector'];
+  
+	// if current role is not admin, inspector but [ADA] Management
+	if (rangerRoles.indexOf(ROLE()) !== -1) {
+    	// ranger fields
+		//SETREADONLY('km', true);
+      	//SETREADONLY('tree_no', true);
+       	//SETREADONLY('status', true);
+        //SETREADONLY('species', true);
+        //SETREADONLY('planted_tree', true);
+        //SETREADONLY('height_m', true);
+      	//SETREADONLY('spread_m', true);
+      	//SETREADONLY('height_m', true);
+      	//SETREADONLY('dbh_cm', true);
+      	//SETREADONLY('dbh2_cm', true);
+  		//SETREADONLY('health_re_inspection',true);
+		//SETREADONLY('health',true);
+        //SETREADONLY('indicator',true);
+       	//SETREADONLY('dbh_2_cm', true);
+      	//SETREADONLY('dbh_3_cm', true);
+	    //SETREADONLY('structure',true);
+   		//SETSTATUSHIDDEN(true);
+      	//SETREADONLY('impact_of_ground_water',true);
+      	//SETREADONLY('maintenance_action',true);
+	    //SETREADONLY('maintenance_required',true);
+      	//SETREADONLY('status_of_maintenance_action',true);
+       	//SETREADONLY('date',true);
+       	//SETREADONLY('maintenance_action',true);
+		//SETREADONLY('irrigation_present',true);
+        //SETREADONLY('comments',true);
+        //SETREADONLY('photos', true);
+      	SETSTATUSFILTER(['1','4']);
+      	//set accuracy to a minimum of 20m
+        var config = {
+	        //auto_sync_enabled: true,
+	        //auto_location_enabled: true,
+	        //auto_location_minimum_accuracy: 20,
+	        manual_location_enabled: false
+	        //media_gallery_enabled: false,
+	        //media_capture_enabled: true,
+	        //photo_quality: '2048',
+	        //video_quality: '720p',
+	        //drafts_enabled: false,
+	        //edit_locations_enabled: true,
+	        //edit_durations_enabled: true
+        };
+
+        SETFORMATTRIBUTES(config);
+
+		//ALERT('Sorry!', 'You only change the status!');
+    	//INVALID('Sorry! You are not allowed to create records in this app.');
+      	//validate loaction
+		lat = LATITUDE();
+		lng = LONGITUDE();
+      	var location = CURRENTLOCATION();
+      	if (!location) 
+      		{
+      		ALERT('No Location Available');
+      		return;
+      		}
+      	var latitude = location.latitude;
+      	var longitude = location.longitude;
+      	var minLatitude = lat - 0.000300;
+      	var maxLatitude = lat + 0.000300;
+      	var minLongitude = lng - 0.000300;
+      	var maxLongitude = lng + 0.000300;
+      
+		if (!(latitude <= maxLatitude && latitude >= minLatitude && longitude <= maxLongitude && longitude >= minLongitude)) 
+      	{
+    		INVALID("It looks like you are not close to the tree to make changes.");
+    		SETSTATUSREADONLY(true);
+      	}
+	}
+
+	// if the current role is one of the designated admin roles...
+  	if (adminRoles.indexOf(ROLE()) !== -1) 
+    {
+	//do nothing
+     SETSTATUSFILTER(null);
+  	}
+  
+});
+
+ON('edit-record', function(event) {
+ 	var adminRoles = ['Inspector','Owner'];
+  	var rangerRoles = ['[ADA] Site Inspector'];
+  
+	// if current role is not admin, inspector but [ADA] Management
+	if (rangerRoles.indexOf(ROLE()) !== -1) 
+    {
+        // ranger fields
+		//SETREADONLY('km', true);
+      	//SETREADONLY('tree_no', true);
+       	//SETREADONLY('status', true);
+		//SETREADONLY('species', true);
+		//SETREADONLY('planted_tree', true);
+        //SETREADONLY('height_m', true);
+      	//SETREADONLY('spread_m', true);
+      	//SETREADONLY('height_m', true);
+      	//SETREADONLY('dbh_cm', true);
+      	//SETREADONLY('dbh2_cm', true);
+  		//SETREADONLY('health_re_inspection',true);
+		//SETREADONLY('health',true);
+		//SETREADONLY('indicator',true);
+       	//SETREADONLY('dbh_2_cm', true);
+      	//SETREADONLY('dbh_3_cm', true);
+	    //SETREADONLY('structure',true);
+   		//SETSTATUSHIDDEN(true);
+		//SETREADONLY('impact_of_ground_water',true);
+		//SETREADONLY('maintenance_action',true);
+		//SETREADONLY('maintenance_required',true);
+		//SETREADONLY('status_of_maintenance_action',true);
+       	//SETREADONLY('date',true);
+		//SETREADONLY('irrigation_present',true);
+		//SETREADONLY('comments',true);
+		//SETREADONLY('photos', true);
+      	SETSTATUSFILTER(['1','4']);
+		var config = {
+	        //auto_sync_enabled: true,
+	        //auto_location_enabled: true,
+	        //auto_location_minimum_accuracy: 20,
+	        manual_location_enabled: false,
+	        //media_gallery_enabled: false,
+	        //media_capture_enabled: true,
+	        //photo_quality: '2048',
+	        //video_quality: '720p',
+	        //drafts_enabled: false,
+	        //edit_locations_enabled: true,
+	        //edit_durations_enabled: true
+        };
+        SETFORMATTRIBUTES(config);
+		//ALERT('Sorry!', 'You only change the status!');
+    	//INVALID('Sorry! You are not allowed to create records in this app.');
+      
+      }
+  	// if the current role is one of the designated admin roles...
+  	if (adminRoles.indexOf(ROLE()) !== -1) 
+    {
+	//do nothing
+    SETSTATUSFILTER(null);
+  	}
+});
+
+
+
+
+///[ADA] Site Office
+//allow user [ADA] Site Office to ONLY change status to red green and orange
+ON('new-record', function(event) {
+ 	var adminRoles = ['Inspector','Owner'];
+  	var rangerRoles = ['[ADA] Site Office'];
+  
+	// if current role is not admin, inspector but [ADA] Site
+	if (rangerRoles.indexOf(ROLE()) !== -1) 
+    {
+    	// ranger fields
+		SETHIDDEN('km', true);
+      	SETHIDDEN('tree_no', true);
+       	SETHIDDEN('status', true);
+        SETHIDDEN('species', true);
+        SETHIDDEN('planted_tree', true);
+        SETHIDDEN('height_m', true);
+      	SETHIDDEN('spread_m', true);
+      	SETHIDDEN('height_m', true);
+      	SETHIDDEN('dbh_cm', true);
+		SETHIDDEN('health',true);
+   		SETHIDDEN('health_re_inspection',true);
+        SETHIDDEN('indicator',true);
+        SETHIDDEN('irrigation_present',true);
+      	SETHIDDEN('structure',true);
+      	SETSTATUSHIDDEN(true);
+      	SETHIDDEN('impact_of_ground_water',true);
+       	SETHIDDEN('maintenance_action',true);
+        SETHIDDEN('comments',true);
+		SETHIDDEN('photos', true);
+		ALERT('Sorry!', 'You cannot create a record in this app. ');
+    	INVALID('Sorry! You are not allowed to create records in this app.');
+   }
+  	// if the current role is one of the designated admin roles...
+  	if (adminRoles.indexOf(ROLE()) !== -1) 
+    {
+	//do nothing
+  	}
+  
+});
+
+
+ON('validate-record', function(event) 
+   {
+ 	var adminRoles = ['Inspector','Owner'];
+  	var rangerRoles = ['[ADA] Site Office'];
+  
+	// if current role is not admin, inspector but [ADA] Management
+	if (rangerRoles.indexOf(ROLE()) !== -1) {
+    	// ranger fields
+		SETREADONLY('km', true);
+      	SETREADONLY('tree_no', true);
+       	//SETREADONLY('status', true);
+        //SETREADONLY('species', true);
+        SETREADONLY('planted_tree', true);
+        SETREADONLY('height_m', true);
+      	SETREADONLY('spread_m', true);
+      	SETREADONLY('height_m', true);
+      	SETREADONLY('dbh_cm', true);
+      	SETREADONLY('dbh2_cm', true);
+		SETREADONLY('health_re_inspection',true);
+		SETREADONLY('health',true);
+        SETREADONLY('indicator',true);
+       	SETREADONLY('dbh_2_cm', true);
+      	SETREADONLY('dbh_3_cm', true);
+	    SETREADONLY('structure',true);
+   		//SETSTATUSHIDDEN(true);
+      	SETREADONLY('impact_of_ground_water',true);
+      	SETREADONLY('maintenance_action',true);
+	    SETREADONLY('maintenance_required',true);
+      	SETREADONLY('status_of_maintenance_action',true);
+       	SETREADONLY('date',true);
+       	SETREADONLY('maintenance_action',true);
+        SETREADONLY('irrigation_present',true);
+        SETREADONLY('comments',true);
+        SETREADONLY('photos', true);
+      	SETSTATUSFILTER(['1','2']);
+		var config = {
+        //auto_sync_enabled: true,
+        //auto_location_enabled: true,
+        //auto_location_minimum_accuracy: 20,
+        manual_location_enabled: false,
+        //media_gallery_enabled: false,
+        //media_capture_enabled: true,
+        //photo_quality: '2048',
+        //video_quality: '720p',
+        //drafts_enabled: false,
+        //edit_locations_enabled: true,
+        //edit_durations_enabled: true
+        };
+        SETFORMATTRIBUTES(config);
+		//ALERT('Sorry!', 'You only change the status!');
+    	//INVALID('Sorry! You are not allowed to create records in this app.');
+      lat = LATITUDE();
+      lng = LONGITUDE();
+      var location = CURRENTLOCATION();
+      if (!location) 
+      {
+      ALERT('No Location Available');
+      return;
+      }
+      var latitude = location.latitude;
+      var longitude = location.longitude;
+      var minLatitude = lat - 0.000300;
+      var maxLatitude = lat + 0.000300;
+      var minLongitude = lng - 0.000300;
+      var maxLongitude = lng + 0.000300;
+      
+      if (!(latitude <= maxLatitude && latitude >= minLatitude && longitude <= maxLongitude && longitude >= minLongitude)) 
+      {
+    INVALID("It looks like you are not close to the tree to make changes.");
+    SETSTATUSREADONLY(true);
+      }
+      
+}
+  // if the current role is one of the designated admin roles...
+  	if (adminRoles.indexOf(ROLE()) !== -1) 
+    {
+	//do nothing
+     SETSTATUSFILTER(null);
+  	}
+  
+});
+
+ON('edit-record', function(event) {
+ 	var adminRoles = ['Inspector','Owner'];
+  	var rangerRoles = ['[ADA] Site Office'];
+  
+	// if current role is not admin, inspector but [ADA] Management
+	if (rangerRoles.indexOf(ROLE()) !== -1) 
+    {
+        // ranger fields
+		SETREADONLY('km', true);
+      	SETHIDDEN('tree_no', true);
+       	//SETREADONLY('status', true);
+        //SETREADONLY('species', true);
+        SETREADONLY('planted_tree', true);
+        SETREADONLY('height_m', true);
+      	SETREADONLY('spread_m', true);
+      	SETREADONLY('height_m', true);
+      	SETREADONLY('dbh_cm', true);
+      	SETREADONLY('dbh2_cm', true);
+	    SETREADONLY('health_re_inspection',true);
+		SETREADONLY('health',true);
+        SETREADONLY('indicator',true);
+       	SETREADONLY('dbh_2_cm', true);
+      	SETREADONLY('dbh_3_cm', true);
+	    SETREADONLY('structure',true);
+   		//SETSTATUSHIDDEN(true);
+      	SETREADONLY('impact_of_ground_water',true);
+       	SETREADONLY('maintenance_action',true);
+	    SETREADONLY('maintenance_required',true);
+      	SETREADONLY('status_of_maintenance_action',true);
+       	SETREADONLY('date',true);
+        SETREADONLY('irrigation_present',true);
+        SETREADONLY('comments',true);
+        SETREADONLY('photos', true);
+      	SETSTATUSFILTER(['1','2']);
+      	var config = {
+	        //auto_sync_enabled: true,
+	        //auto_location_enabled: true,
+	        //auto_location_minimum_accuracy: 20,
+	        manual_location_enabled: false
+	        //media_gallery_enabled: false,
+	        //media_capture_enabled: true,
+	        //photo_quality: '2048',
+	        //video_quality: '720p',
+	        //drafts_enabled: false,
+	        //edit_locations_enabled: true,
+	        //edit_durations_enabled: true
+        };
+        SETFORMATTRIBUTES(config);
+		//ALERT('Sorry!', 'You only change the status!');
+    	//INVALID('Sorry! You are not allowed to create records in this app.');
+      
+      }
+  	// if the current role is one of the designated admin roles...
+  	if (adminRoles.indexOf(ROLE()) !== -1) 
+    {
+	//do nothing
+    SETSTATUSFILTER(null);
+  	}
+});
