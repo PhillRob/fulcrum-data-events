7002313e0f6737ff2892b6fe215228ae6dc3d815
Philipp <robeck-p@email.ulster.ac.uk>
2023-05-19 01:33:25 +0300

    update to remove listed

--- fulcrum-file-delete.sh
+++ fulcrum-file-delete.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-## deletes all files not in NOT listed in CVS
-INPUT = data.csv
+## deletes all files not in NOT listed in CSV
+INPUT=del.csv
 OLDIFS=$IFS
+COUNT=0
 IFS=,
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 while read -a csv_line
 do
-	find  -not -name ${csv_line[0]}.pdf -type f -exec rm -v {} \;
+	# echo "${csv_line[0]}.jpg file found"
+	let COUNT=COUNT+1
+	echo $COUNT
+	# echo ${csv_line[0]}.jpg
+	find /Users/philipp/Downloads/gr_tree_assessment . -name ${csv_line[0]}.jpg -type f -exec rm -v {} \;
+	# find -not -name ${csv_line[0]}.jpg -type f -exec rm -v {} \;
 done < $INPUT
+
